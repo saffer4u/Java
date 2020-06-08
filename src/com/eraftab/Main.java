49a6d1e83736c27d0c760e9951ce1455a65044b8
saffer4u <aftab.alam4u71@gmail.com>
2020-06-08 09:18:02 +0530

    Datatypes

--- src/com/eraftab/Main.java
+++ src/com/eraftab/Main.java
@@ -3,8 +3,21 @@ package com.eraftab;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        //I'm learning Java.
-        System.out.println("I am learning Java");
+        System.out.println("Data Types");
+
+        /*
+        Data types :
+            1. Primitive datatypes :
+                byte(1byte),Short(2byte),int(4byte),long(8byte),float(4byte),double (8byte),boolean(1bit),char(2byte).
+
+            2. Non Primitive :
+                we can run methods on non primitive data type;
+         */
+
+
+        char grade = 'A';
+        System.out.println(grade);
+        double pi = 3.143818929482920394d;
+        System.out.println(pi);
     }
 }
